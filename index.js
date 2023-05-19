f479298d3cce65897368aba5716c974814caf621
Rafatulislam60 <rafatulislam67@gmail.com>
2023-05-20 00:10:58 +0600

    allToys service add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,11 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 console.log(process.env.DB_PASS);
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v7y5nyp.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v7y5nyp.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,6 +26,38 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db("kidToys").collection("toys");
+
+    app.get("/allToys", async (req, res) => {
+      const cursor = toysCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/allToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const options = {
+        // sort matched documents in descending order by rating
+        sort: { "imdb.rating": -1 },
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: {
+          seller: 1,
+          availableQuantity: 1,
+          picture: 1,
+          rating: 1,
+          price: 1,
+          toyName: 1,
+          detailsDescription: 1,
+        },
+      };
+
+      const result = await toysCollection.findOne(query, options);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -40,7 +70,7 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get("/allToys", (req, res) => {
+app.get("/", (req, res) => {
   res.send("toys are running");
 });
 
